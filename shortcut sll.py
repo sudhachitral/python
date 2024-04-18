c41044a0c233777f0f6016066fef2f1e274a094b
sudhachitral <167319015+sudhachitral@users.noreply.github.com>
2024-04-18 14:55:07 +0530

    Create shortcut sll.py

---
+++ shortcut sll.py
@@ -0,0 +1,24 @@
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+def printLinkedList(head):
+        curr=head
+        while curr!=None:
+            print(curr.data,end="-->")
+            curr=curr.next
+        print(None)
+def insertAtEndOfTail(head, ele):
+    newBlock=Node(ele)
+    if head==None:
+        return newBlock
+    curr=head
+    while curr.next!=None:
+        curr=curr.next
+    curr.next=newBlock
+    return head
+l=[11,22,33,44,55,66,77,88,99,110]
+head=None
+for ele in l:
+    head=insertAtEndOfTail(head,ele)
+printLinkedList(head)
