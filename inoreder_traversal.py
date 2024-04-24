bf88f57a63faa17a5f18d1f4b5bcdeabc0694b9f
sudhachitral <167319015+sudhachitral@users.noreply.github.com>
2024-04-24 12:40:00 +0530

    Create inoreder_traversal.py

---
+++ inoreder_traversal.py
@@ -0,0 +1,54 @@
+class TreeNode:
+    def __init__(self, data):
+        self.data = data 
+        self.left = None 
+        self.right = None 
+ 
+ 
+def printInOrder(root):
+    if root == None:
+        return
+ 
+
+   
+    printInOrder(root.left)
+    print(root.data, end = " ")
+    printInOrder(root.right)
+root = TreeNode(11)
+obj2 = TreeNode(7)
+obj3 = TreeNode(15)
+obj4 = TreeNode(5)
+obj5 = TreeNode(9)
+obj6 = TreeNode(13)
+obj7 = TreeNode(20)
+obj8=TreeNode(3)
+obj9=TreeNode(8)
+obj10=TreeNode(10)
+obj11=TreeNode(12)
+obj12=TreeNode(14)
+obj13=TreeNode(18)
+obj14=TreeNode(25)
+
+
+root.left = obj2 
+root.right = obj3 
+ 
+obj2.left = obj4 
+obj2.right = obj5 
+ 
+obj3.left = obj6 
+obj3.right = obj7
+
+obj4.left=obj8
+
+
+obj5.left=obj9
+obj5.right=obj10
+
+obj6.left=obj11
+obj6.right=obj12
+
+obj7.left=obj13
+obj7.right=obj14
+
+printInOrder(root)
