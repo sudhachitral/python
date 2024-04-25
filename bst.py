0a2dea594881af7231457682dae04d64bf062bcb
sudhachitral <167319015+sudhachitral@users.noreply.github.com>
2024-04-25 22:00:04 +0530

    Create bst.py

---
+++ bst.py
@@ -0,0 +1,34 @@
+class TreeNode:
+     def __init__(self, data):
+        self.data = data 
+        self.right = None 
+        self.left = None  
+ 
+def insertIntoBST(root, ele):
+    if root == None:
+        newBlock = TreeNode(ele)
+        return newBlock 
+ 
+    if root.data < ele:
+        root.right = insertIntoBST(root.right, ele)
+    else:
+        root.left = insertIntoBST(root.left, ele)
+    return root
+ 
+def printInorderTraversal(root):
+    if root == None:
+        return 
+ 
+    printInorderTraversal(root.left)
+    print(root.data, end = " ")
+    printInorderTraversal(root.right)
+ 
+ 
+n=int(input()) 
+nums = list(map(int,input().split()))
+ 
+root = None 
+for ele in nums:
+    root = insertIntoBST(root, ele)
+ 
+printInorderTraversal(root)
